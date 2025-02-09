322ccd87db5e0e2fb6ef9815833f79f003c0cd32
Asha Saini <sainiasha.ash@gmail.com>
2025-02-10 00:57:02 +0530

    Set up Vite with React portfolio, starter UI & loading screen

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import './index.css';
+import { LoadingScreen } from './components/LoadingScreen'
+import { useState } from 'react';
+
+function App() {
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <>
+      { !isLoaded && < LoadingScreen onComplete={() => setIsLoaded(true)} />
+      }
+    </>
+  )
+}
+
+export default App
